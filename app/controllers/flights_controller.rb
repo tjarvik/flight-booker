76ab02bf3bfea9da9f42697a73087859bd37bd23
Tyler Jarvik <tjarvik@gmail.com>
2019-06-04 22:32:13 -0700

    add flight selection

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,22 @@
 class FlightsController < ApplicationController
   def index
+    
     @airports = Airport.all.map{ |a| [ a.code, a.id ] }
-    @dates = Flight.all.map{ |f| [ f.departure.strftime("%m/%d/%Y"), f.departure.strftime("%m/%d/%Y") ] }.uniq.sort
-    @passenger_counts = [["1",1], ["2",2], ["3",3], ["4",4],]
+    @months = Flight.all.map{ |f| [ f.departure.strftime("%m"), f.departure.strftime("%m") ] }.uniq.sort
+    @days = Flight.all.map{ |f| [ f.departure.strftime("%d"), f.departure.strftime("%d") ] }.uniq.sort
+    @years = Flight.all.map{ |f| [ f.departure.strftime("%Y"), f.departure.strftime("%Y") ] }.uniq.sort
+    @num_passengers = [["1",1], ["2",2], ["3",3], ["4",4]]
+
+    if params[:from_airport]
+      date = DateTime.new(params[:year].to_i, params[:month].to_i, params[:day].to_i, 0, 0, 0, '-8')
+      @flights = Flight.where(departure: date..(date + 1.day)).where(
+                              "from_airport_id = ? AND
+                               to_airport_id = ?", 
+                               params[:from_airport].to_i,
+                               params[:to_airport].to_i)
+      @selected_num_passengers = params[:num_passengers]
+
+    end
   end
 
   
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,10 +1,5 @@
 class Flight < ApplicationRecord
     belongs_to :from_airport, class_name: "Airport"
     belongs_to :to_airport, class_name: "Airport"
-
-    def self.get_flight_dates
-        flight_dates = UgradRsvp.find_by_sql("select distinct event_date from admissions.ugrad_rsvps where event_date is not null order by event_date desc")
-    end 
-
     
 end
